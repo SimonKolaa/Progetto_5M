36869565621a9bc1bc3eae105d3a91b2550bc3ee
SimonKola <147493438+SimonKolaa@users.noreply.github.com>
2026-01-07 15:37:45 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-# Application Factory Pattern 
+#Application Factory Pattern 
 
 import os
 from flask import Flask
@@ -7,23 +7,23 @@ def create_app():
     #Creo l'istanza di Flask
     app = Flask(__name__, instance_relative_config=True)
 
-    # 2. Configurazione di base
+    #Configurazione di base
     app.config.from_mapping(
-        SECRET_KEY='dev',  # In produzione va cambiata!
+        SECRET_KEY='dev',  #in produzione va cambiata!
         DATABASE=os.path.join(app.instance_path, 'walletwise.sqlite'),
     )
 
-    # Assicurati che la cartella instance esista
+    #in modo che la cartella instance esista
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # 3. Inizializza database (02_DB_e_Repository.md)
+    #inizializza database (02_DB_e_Repository.md)
     from . import db
     db.init_app(app)
 
-    # 4. Registrazione Blueprints (03_Blueprints_e_Routing.md)
+    #registrazione Blueprints (03_Blueprints_e_Routing.md)
     from . import auth
     from . import main
     
